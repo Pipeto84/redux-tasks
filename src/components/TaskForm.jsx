ec86b42c7f9829e4c1fc816e33ee3feeb7feeac4
Pipe Jara <pipejarauribeboteroa@gmail.com>
2025-03-03 10:06:19 -0500

    TailwindCSS ready

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTask,editTask } from "../features/tasks/taskSlice";
+import { addTask, editTask } from "../features/tasks/taskSlice";
 import { v4 as uuid } from "uuid";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -37,27 +37,35 @@ export const TaskForm = () => {
     navigate("/");
   };
   useEffect(() => {
-    if (params) {
+    if (params.id) {
       setTask(tasks.find((task) => task.id === params.id));
     }
-  }, []);
+  }, [params.id, tasks]);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="title" className="block text-xs font-bold mb-2">
+        Task:
+      </label>
       <input
         name="title"
         type="text"
-        placeholder="Title"
+        placeholder="Title..."
         onChange={handleChange}
         value={task.title}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       />
+      <label htmlFor="description" className="block text-xs font-bold mb-2">
+        Description:
+      </label>
       <textarea
         name="description"
-        placeholder="Description"
+        placeholder="Description..."
         onChange={handleChange}
         value={task.description}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       ></textarea>
-      <button>Save</button>
+      <button className="bg-indigo-600 px-2 py-1 rounded-md">Save</button>
     </form>
   );
 };
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,19 +9,30 @@ export const TaskList = () => {
     dispatch(deleteTask(id));
   };
   return (
-    <div>
-      <header>
-        <h1>Tasks {tasks.length}</h1>
-        <Link to="/create-task">Create Task</Link>
+    <div className="w-4/6">
+      <header className="flex justify-between items-center py-4">
+        <h1 className="font-bold">Tasks: {tasks.length}</h1>
+        <Link
+          to="/create-task"
+          className="bg-indigo-600 px-2 py-1 rounded-sm text-sm"
+        >
+          Create Task
+        </Link>
       </header>
-      {tasks.map((task) => (
-        <div key={task.id}>
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-          <Link to={`/edit-task/${task.id}`}>Edit</Link>
-          <button onClick={() => handleDelete(task.id)}>Delete</button>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {tasks.map((task) => (
+          <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+            <header className="flex justify-between">
+              <h3>{task.title}</h3>
+              <div className="flex gap-x-2">
+                <Link to={`/edit-task/${task.id}`}className="bg-zinc-600 px-2 py-1 text-xs rounded-md">Edit</Link>
+                <button onClick={() => handleDelete(task.id)} className="bg-red-500 px-2 py-1 text-xs rounded-md ">Delete</button>
+              </div>
+            </header>
+            <p>{task.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
